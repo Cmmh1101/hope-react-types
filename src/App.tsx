cb1272b0ed958c9743b3de428ff665a209aa54e3
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-02-08 22:36:47 -0600

    initial reactstrap, react-router installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Container, Row, Col } from "reactstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="text-danger">App using reactstrap</h1>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
