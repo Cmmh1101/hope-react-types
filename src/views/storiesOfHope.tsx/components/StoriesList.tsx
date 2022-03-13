fe0e605049d839e7b8eec729f4a83a1345640ea2
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-03-12 19:14:52 -0600

    storiesList styles

--- src/views/storiesOfHope.tsx/components/StoriesList.tsx
+++ src/views/storiesOfHope.tsx/components/StoriesList.tsx
@@ -27,7 +27,7 @@ const ProgramsList = () => {
               >
                 <Col xs={12} md={6} className="p-0 stories-list-img-containter">
                   <div
-                    className="w-100 shadow"
+                    className="w-100"
                     style={{
                       backgroundImage: `url(${article.image})`,
                       backgroundPosition: "center",
