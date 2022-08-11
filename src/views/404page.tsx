e146a16913a02f9b5da3825d6c4e659c0d2d2f24
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-08-10 22:04:08 -0500

    add: page not found content

--- src/views/404page.tsx
+++ src/views/404page.tsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+import { Col, Container, Row } from "reactstrap";
+
 const NotFoundPage = () => {
-  return <h1>404: Page Not Found</h1>;
+  return (
+    <Container className="not-found-page">
+      <Row>
+        <Col xs={12} className="not-found-img">
+          <img src="../../../images/not-found.svg" alt="not found" />
+        </Col>
+        <Col xs={12} className="not-found-text">
+          <h4>Page not found</h4>
+          <Link to="/">Home</Link>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default NotFoundPage;
