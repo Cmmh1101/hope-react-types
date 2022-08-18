d022d94347cd6f0ccff53db7bed53ada166f4ddc
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-08-18 00:08:04 -0500

    change: verse render method

--- src/utils/BibleVerses.tsx
+++ src/utils/BibleVerses.tsx
@@ -1,28 +1,31 @@
+import { useEffect, useState } from "react";
 import { Col } from "reactstrap";
 import verses from "../../src/bibleVerses";
 
+interface IVerse {
+  verse: string;
+  reference: string;
+}
+
 const BibleVerses = () => {
+  const [activeVerse, setActiveVerse] = useState<IVerse | undefined>();
   const totalVerses = verses.length;
-  const activeVerses = verses[Math.floor(Math.random() * totalVerses)];
-  console.log(activeVerses);
+
+  // const activeVerses = verses[Math.floor(Math.random() * totalVerses)];
+  // console.log(activeVerses);
+
+  useEffect(() => {
+    const verse = verses[Math.floor(Math.random() * totalVerses)];
+    setActiveVerse(verse);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Col xs={12} className="donate-verse-img mb-5 mb-md-0">
       <Col xs={12} className="verse order-lg-2">
-        <p>{activeVerses.verse}</p>
-        <span>{activeVerses.reference}</span>
+        <p>{activeVerse?.verse}</p>
+        <span>{activeVerse?.reference}</span>
       </Col>
-      {/* <Col
-        xs={12}
-        lg={2}
-        className="d-flex order-lg-1 align-items-center justify-content-center"
-      >
-        <img
-          src="../../images/bibleSun.png"
-          alt="bible"
-          className="bible-image"
-        />
-      </Col> */}
     </Col>
   );
 };
